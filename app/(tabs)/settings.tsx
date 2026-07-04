ad52209ec2c43346d08b19132557b59d0f613cdd
sukkoth <suukootj@gmail.com>
2026-07-04 21:31:58 +0300

    dep: add prettier

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function SettingsScreen() {
   return (
-    <View className="flex-1 justify-center items-center">
+    <View className="flex-1 items-center justify-center">
       <Text>Settings</Text>
     </View>
   );
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,11 @@
-import { Stack } from 'expo-router';
-import './global.css';
+import { Stack } from "expo-router";
+import "./global.css";
 
 export default function RootLayout() {
-    return (
-        <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen
-                name="reading"
-                options={{ headerShown: true, title: 'Reading' }}
-            />
-        </Stack>
-    );
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="reading" options={{ headerShown: true, title: "Reading" }} />
+    </Stack>
+  );
 }
